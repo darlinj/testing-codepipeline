4f252872a5aa6db1f898201127e930e8c911705a
Joe Darling <joe.darling@bt.com>
2020-12-29 15:35:34 +0000

    Adding users via script

--- createCognitoUser.sh
+++ createCognitoUser.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 
-aws cognito-idp sign-up --username user1 --password Passw0rd! --client-id 7ltjdkpko1ohr8tf5v8ncnl0in  --profile admin --user-attributes Name=phone_number,Value="+441473123456" Name=email,Value="joe@example.com"
-aws cognito-idp admin-set-user-password --user-pool-id eu-west-1_1i1dG5iTo --username user1 --password Passw0rd! --permanent  --profile admin
+#USERNAME=chicken
+USERNAME=${TEST_USERNAME}
+PASSWORD=Passw0rd!
+#CLIENT_ID=7ltjdkpko1ohr8tf5v8ncnl0in  
+#USER_POOL_ID=eu-west-1_1i1dG5iTo
+
+aws cognito-idp sign-up --username ${USERNAME} --password ${PASSWORD} --client-id ${CLIENT_ID} --profile admin --user-attributes Name=phone_number,Value="+441473123456" Name=email,Value="joe@example.com"
+aws cognito-idp admin-set-user-password --user-pool-id ${USER_POOL_ID} --username ${USERNAME} --password ${PASSWORD} --permanent --profile admin
