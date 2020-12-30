01968d038bcdc92cafba174b5c368c3b13dbd1bf
Joe Darling <joe.darling@bt.com>
2020-12-30 09:05:31 +0000

    Single API test failing successfully

--- api_tests.spec.js
+++ api_tests.spec.js
@@ -1,26 +1,49 @@
-import Amplify, { API, graphqlOperation } from "aws-amplify";
+import Amplify, { API, graphqlOperation, Auth } from "aws-amplify";
 
 const aws_config = {
+  aws_project_region: "eu-west-1",
   aws_appsync_graphqlEndpoint: process.env.APIEndpoint,
   aws_appsync_region: "eu-west-1",
-  aws_appsync_authenticationType: "API_KEY",
-  aws_appsync_apiKey: "da2-mc2r73rx5vhufhhtnver62edwu"
+  aws_appsync_authenticationType: "AMAZON_COGNITO_USER_POOLS",
+  aws_appsync_apiKey: "null",
+  aws_cognito_region: "eu-west-1",
+  aws_user_pools_id: process.env.USER_POOL_ID,
+  authenticationFlowType: "USER_PASSWORD_AUTH",
+  aws_user_pools_web_client_id: process.env.CLIENT_ID
 };
+
+const password = "Passw0rd!";
 
 Amplify.configure(aws_config);
 
-it("fetches things", () => {
+beforeEach(async () => {
+  console.log("Config", aws_config);
+  console.log("User", process.env.TEST_USERNAME);
+  console.log("Password", password);
+  await Auth.signIn(process.env.TEST_USERNAME, password).then(
+    user => {
+      console.log(user);
+    },
+    error => console.log("FAILED!!!!", error)
+  );
+});
+
+it("fetches things", async () => {
   console.log("API Endpoint: ", process.env.APIEndpoint);
   const query = graphqlOperation(`query MyQuery {
-  get(id: "123", meta: "somedata", title: "sdfds") {
-    meta
-    id
-    title
+    getQuestionnaires {
+    questionnaires {
+      content
+    }
   }
 }`);
-  API.graphql(query).then(result => {
-    expect(result.data["get"]["meta"]).toEqual(
-      "Meta stuff: somedata Inserted text"
-    );
-  });
+  await API.graphql(query)
+    .then(result => {
+      console.log("result", result);
+      expect(result.data).toEqual("Meta stuff: somedata Inserted text");
+    })
+    .catch(err => {
+      console.log(err);
+      throw err;
+    });
 });
