1e5b894865afacc7442076cf4a888ed34cc46245
Joe Darling <joe.darling@bt.com>
2021-01-05 14:42:01 +0000

    Add a script to sign in a local user so they can run CLI commands

--- infra/createCognitoUser.js
+++ infra/createCognitoUser.js
@@ -2,51 +2,6 @@ import AWS from "aws-sdk";
 AWS.config.region = "eu-west-1";
 const sts = new AWS.STS();
 
-//const login = () => {
-//  console.log(process.argv[2]);
-//  var params = {
-//    DurationSeconds: "3600",
-//    SerialNumber: "arn:aws:iam::919384593253:mfa/joe.darling",
-//    TokenCode: process.argv[2]
-//  };
-//  sts.getSessionToken(params, function(err, data) {
-//    if (err) console.log("failed to getSessionToken", err, err.stack);
-//    else {
-//      console.log(data); // successful response
-//      assumeRole();
-//      AWS.config.update({
-//        accessKeyId: data.Credentials.AccessKeyId,
-//        secretAccessKey: data.Credentials.SecretAccessKey,
-//        sessionToken: data.Credentials.SessionToken
-//      });
-//    }
-//  });
-//};
-
-const assumeRole = () => {
-  sts.assumeRole(
-    {
-      RoleArn: "aws:iam::562925407179:role/bttv-mnb-poc-role-admin",
-      RoleSessionName: "awssdk",
-      SerialNumber: "arn:aws:iam::919384593253:mfa/joe.darling",
-      TokenCode: process.argv[2]
-    },
-    function(err, data) {
-      if (err) {
-        console.log("Cannot assume role");
-        console.log(err, err.stack);
-      } else {
-        AWS.config.update({
-          accessKeyId: data.Credentials.AccessKeyId,
-          secretAccessKey: data.Credentials.SecretAccessKey,
-          sessionToken: data.Credentials.SessionToken
-        });
-        checkIfUserExists();
-      }
-    }
-  );
-};
-
 let cognitoidentityserviceprovider = new AWS.CognitoIdentityServiceProvider({
   apiVersion: "2016-04-18",
   region: "eu-west-1"
@@ -54,12 +9,11 @@ let cognitoidentityserviceprovider = new AWS.CognitoIdentityServiceProvider({
 
 const checkIfUserExists = () => {
   var params = {
-    UserPoolId: process.env.UserPoolId,
-    AttributesToGet: ["name"]
+    UserPoolId: process.env.UserPoolId
   };
   cognitoidentityserviceprovider.listUsers(params, function(err, data) {
     if (err) console.log(err, err.stack);
-    else console.log(data); // successful response
+    else console.log(data); 
   });
 };
 
@@ -94,7 +48,7 @@ const makeCognitoUser = () => {
 };
 
 const makeTestUser = () => {
-  assumeRole();
+  checkIfUserExists();
 };
 
 makeTestUser();
