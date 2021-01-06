989256b332addd7a5f78f1bfdd83bc996c5ec369
Joe Darling <joe.darling@bt.com>
2021-01-06 16:52:36 +0000

    move api calls into an API file

--- api/tests/questionnaireAPI.spec.js
+++ api/tests/questionnaireAPI.spec.js
@@ -1,5 +1,11 @@
-import Amplify, { API, graphqlOperation, Auth } from "aws-amplify";
+import Amplify, { Auth } from "aws-amplify";
 import { clearDatabase, addQuestionnaireForAnotherUser } from "./DBAdmin";
+import {
+  saveQuestionnaire,
+  getQuestionnaire,
+  deleteQuestionnaire,
+  getQuestionnaires
+} from "../../src/apiCalls.js";
 import awsConfig from "../../aws_config";
 
 Amplify.configure(awsConfig);
@@ -13,49 +19,6 @@ const login = async () => {
   await Auth.signIn(process.env.TEST_USERNAME, process.env.TEST_USER_PASSWORD);
 };
 
-const runGraphqlOperation = query_string => {
-  const query = graphqlOperation(query_string);
-  return API.graphql(query);
-};
-
-const getQuestionnaires = () => {
-  return runGraphqlOperation(`query MyQuery {
-  getQuestionnaires {
-    questionnaires {
-      title
-      content
-      QuestionnaireId
-    }
-  }
-}`);
-};
-
-const getQuestionnaire = id => {
-  return runGraphqlOperation(`query MyQuery {
-  getQuestionnaire(QuestionnaireId: "${id}") {
-    title
-    content
-    QuestionnaireId
-  }
-}`);
-};
-
-const deleteQuestionnaire = id => {
-  return runGraphqlOperation(`mutation MyMutation {
-  deleteQuestionnaire(QuestionnaireId: "${id}") {
-    content
-  }
-}`);
-};
-
-const saveQuestionnaire = (id, content, title) => {
-  return runGraphqlOperation(`mutation MyMutation {
-    saveQuestionnaire(content: "${content}", QuestionnaireId: "${id}", title: "${title}") {
-      title
-      content
-    }
-  }`);
-};
 describe("The Questionnaire API", () => {
   it("Returns an empty array if there are no records in the DB", async () => {
     await getQuestionnaires().then(result => {
---
+++ src/apiCalls.js
@@ -0,0 +1,45 @@
+import Amplify, { API, graphqlOperation } from "aws-amplify";
+
+const runGraphqlOperation = query_string => {
+  const query = graphqlOperation(query_string);
+  return API.graphql(query);
+};
+
+export const getQuestionnaires = () => {
+  return runGraphqlOperation(`query MyQuery {
+  getQuestionnaires {
+    questionnaires {
+      title
+      content
+      QuestionnaireId
+    }
+  }
+}`);
+};
+
+export const getQuestionnaire = id => {
+  return runGraphqlOperation(`query MyQuery {
+  getQuestionnaire(QuestionnaireId: "${id}") {
+    title
+    content
+    QuestionnaireId
+  }
+}`);
+};
+
+export const deleteQuestionnaire = id => {
+  return runGraphqlOperation(`mutation MyMutation {
+  deleteQuestionnaire(QuestionnaireId: "${id}") {
+    content
+  }
+}`);
+};
+
+export const saveQuestionnaire = (id, content, title) => {
+  return runGraphqlOperation(`mutation MyMutation {
+    saveQuestionnaire(content: "${content}", QuestionnaireId: "${id}", title: "${title}") {
+      title
+      content
+    }
+  }`);
+};
