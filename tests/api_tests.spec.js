aa93f53a236bf94dc7f6bb8507b3be13fb0f9869
Joe Darling <joe.darling@bt.com>
2021-01-06 15:14:12 +0000

    Test for deleting things that don't exist

--- tests/api_tests.spec.js
+++ tests/api_tests.spec.js
@@ -67,87 +67,94 @@ const saveQuestionnaire = (id, content, title) => {
     }
   }`);
 };
-
-it("Returns an empty array if there are no records in the DB", async () => {
-  await getQuestionnaires().then(result => {
-    expect(result.data).toEqual({
-      getQuestionnaires: {
-        questionnaires: []
-      }
-    });
-  });
-});
-
-it("Adds a questionnaire and then checks it is there", async () => {
-  await saveQuestionnaire("1234", "Some content", "Some title");
-
-  await getQuestionnaires().then(result => {
-    expect(result.data).toEqual({
-      getQuestionnaires: {
-        questionnaires: [
-          {
-            QuestionnaireId: "1234",
-            content: "Some content",
-            title: "Some title"
-          }
-        ]
-      }
-    });
-  });
-});
-
-it("Can't see items that don't belong to this user", async () => {
-  await addQuestionnaireForAnotherUser();
-
-  await saveQuestionnaire("1234", "Some content", "Some title");
-
-  await getQuestionnaires().then(result => {
-    expect(result.data).toEqual({
-      getQuestionnaires: {
-        questionnaires: [
-          {
-            QuestionnaireId: "1234",
-            content: "Some content",
-            title: "Some title"
-          }
-        ]
-      }
-    });
-  });
-});
-
-it("Can get an individual Questionnaire by ID", async () => {
-  await saveQuestionnaire("1234", "Some content", "Some title");
-
-  await getQuestionnaire("1234").then(result => {
-    expect(result.data).toEqual({
-      getQuestionnaire: {
-        QuestionnaireId: "1234",
-        content: "Some content",
-        title: "Some title"
-      }
-    });
-  });
-});
-
-it("Can delete an individual Questionnaire by ID", async () => {
-  await saveQuestionnaire("1234", "Some content", "Some title");
-
-  await getQuestionnaire("1234").then(result => {
-    expect(result.data).toEqual({
-      getQuestionnaire: {
-        QuestionnaireId: "1234",
-        content: "Some content",
-        title: "Some title"
-      }
+describe("The Questionnaire API", () => {
+  it("Returns an empty array if there are no records in the DB", async () => {
+    await getQuestionnaires().then(result => {
+      expect(result.data).toEqual({
+        getQuestionnaires: {
+          questionnaires: []
+        }
+      });
     });
   });
 
-  await deleteQuestionnaire("1234");
+  it("Adds a questionnaire and then checks it is there", async () => {
+    await saveQuestionnaire("1234", "Some content", "Some title");
 
-  await getQuestionnaire("1234").then(result => {
-    expect(result.data).toEqual({
-      getQuestionnaire: null
+    await getQuestionnaires().then(result => {
+      expect(result.data).toEqual({
+        getQuestionnaires: {
+          questionnaires: [
+            {
+              QuestionnaireId: "1234",
+              content: "Some content",
+              title: "Some title"
+            }
+          ]
+        }
+      });
+    });
+  });
+
+  it("Can't see items that don't belong to this user", async () => {
+    await addQuestionnaireForAnotherUser();
+
+    await saveQuestionnaire("1234", "Some content", "Some title");
+
+    await getQuestionnaires().then(result => {
+      expect(result.data).toEqual({
+        getQuestionnaires: {
+          questionnaires: [
+            {
+              QuestionnaireId: "1234",
+              content: "Some content",
+              title: "Some title"
+            }
+          ]
+        }
+      });
+    });
+  });
+
+  it("Can get an individual Questionnaire by ID", async () => {
+    await saveQuestionnaire("1234", "Some content", "Some title");
+
+    await getQuestionnaire("1234").then(result => {
+      expect(result.data).toEqual({
+        getQuestionnaire: {
+          QuestionnaireId: "1234",
+          content: "Some content",
+          title: "Some title"
+        }
+      });
+    });
+  });
+
+  it("Can delete an individual Questionnaire by ID", async () => {
+    await saveQuestionnaire("1234", "Some content", "Some title");
+
+    await getQuestionnaire("1234").then(result => {
+      expect(result.data).toEqual({
+        getQuestionnaire: {
+          QuestionnaireId: "1234",
+          content: "Some content",
+          title: "Some title"
+        }
+      });
+    });
+
+    await deleteQuestionnaire("1234");
+
+    await getQuestionnaire("1234").then(result => {
+      expect(result.data).toEqual({
+        getQuestionnaire: null
+      });
+    });
+  });
+
+  it("deleting a Questionnaire that doesn't exist", async () => {
+    await deleteQuestionnaire("9999").then(result => {
+      expect(result.data).toEqual({ deleteQuestionnaire: null });
     });
   });
 });
