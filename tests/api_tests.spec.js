988425522b159c53c46ab0e9190a72555c564a41
Joe Darling <joe.darling@bt.com>
2021-01-06 14:49:39 +0000

    Added delete test

--- tests/api_tests.spec.js
+++ tests/api_tests.spec.js
@@ -51,6 +51,14 @@ const getQuestionnaire = id => {
 }`);
 };
 
+const deleteQuestionnaire = id => {
+  return runGraphqlOperation(`mutation MyMutation {
+  deleteQuestionnaire(QuestionnaireId: "${id}") {
+    content
+  }
+}`);
+};
+
 const saveQuestionnaire = (id, content, title) => {
   return runGraphqlOperation(`mutation MyMutation {
     saveQuestionnaire(content: "${content}", QuestionnaireId: "${id}", title: "${title}") {
@@ -118,6 +126,28 @@ it("Can get an individual Questionnaire by ID", async () => {
         content: "Some content",
         title: "Some title"
       }
+    });
+  });
+});
+
+it("Can delete an individual Questionnaire by ID", async () => {
+  await saveQuestionnaire("1234", "Some content", "Some title");
+
+  await getQuestionnaire("1234").then(result => {
+    expect(result.data).toEqual({
+      getQuestionnaire: {
+        QuestionnaireId: "1234",
+        content: "Some content",
+        title: "Some title"
+      }
+    });
+  });
+
+  await deleteQuestionnaire("1234");
+
+  await getQuestionnaire("1234").then(result => {
+    expect(result.data).toEqual({
+      getQuestionnaire: null
     });
   });
 });
