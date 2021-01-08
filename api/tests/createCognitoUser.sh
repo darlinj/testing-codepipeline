293bdbba3fc7252b780db0f77283f70314d4d044
Joe Darling <joe.darling@bt.com>
2021-01-08 16:13:20 +0000

    Move to email address for username

--- api/tests/createCognitoUser.sh
+++ api/tests/createCognitoUser.sh
@@ -16,12 +16,12 @@ else
   PROFILE=""
 fi
 if [[ -z "${CLIENT_ID}" ]]; then
-  CLIENT_ID=7ltjdkpko1ohr8tf5v8ncnl0in  
+  echo "FAIL: Cognito CLIENT_ID needs to be set"
 fi
 if [[ -z "${USER_POOL_ID}" ]]; then
-  USER_POOL_ID=eu-west-1_1i1dG5iTo
+  echo "FAIL: Cognito USER_POOL_ID needs to be set"
 fi
 
-aws cognito-idp sign-up --username ${USERNAME} --password ${PASSWORD} --client-id ${CLIENT_ID} --user-attributes Name=phone_number,Value="+441473123456" Name=email,Value="joe@example.com" ${PROFILE}
+aws cognito-idp sign-up --username ${USERNAME} --password ${PASSWORD} --client-id ${CLIENT_ID} --user-attributes Name=phone_number,Value="+441473123456" ${PROFILE}
 
 aws cognito-idp admin-set-user-password --user-pool-id ${USER_POOL_ID} --username ${USERNAME} --password ${PASSWORD} --permanent ${PROFILE}
